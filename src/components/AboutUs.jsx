976566b410aa3cf5605fc7f0a97279838573bf23
Vijay KV <vijaykv2228@gmail.com>
2023-05-13 15:21:19 +0530

    Check

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,6 +7,7 @@ const AboutUs = () => {
     <>
     <div class=" mx-6  bg-green-50 rounded-lg sm:p-8 sm:mx-20 flex justify-center items-center ">
   <div class=" mt-3  ">
+    <h1>Hello HAPPY NEWYEAR</h1>
     <h1 class="text-4xl p-2  font-bold sm:text-7xl text-center sm:w-[700px]">We're changing the whole game.</h1>
     <div className='px-1 py-5  sm:ml-32 sm:mt-3 flex'>
       <button className=' ml-6 outline-double outline-1 bg-black text-white py-2 px-6 rounded-full font-bold transition duration-300 ease-in-out hover:bg-white hover:text-black sm:w-44 sm:h-14'>Get Started</button>
