29cc5d0db8985745e606b14f0159adcfea856f51
Vijay KV <vijaykv2228@gmail.com>
2023-05-13 14:58:30 +0530

    IMAGE UPDATE

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../assets/images/hero.png'
+// import Hero from '../assets/images/hero.png'
 const HeroSection = () => {
   return (
     <div className='p-16 '>
@@ -12,7 +12,7 @@ const HeroSection = () => {
                         <p className='w-[75%] py-8 text-xl'>We all are already abundant. We just need a mentor to help us realise it.</p>
         <button className='outline-double outline-1 py-2 px-8 rounded-full font-semibold bg-black text-white hover:bg-gray-950 hover:text-white'>Get Started</button>
       </div>
-      <img className='lg:-mt-9 mr-16 h-[500px] w-full lg:w-[400px] object-cover rounded-lg lg:rounded-r-lg' src={Hero} alt='hero'/>
+      <img className='lg:-mt-9 mr-16 h-[500px] w-full lg:w-[400px] object-cover rounded-lg lg:rounded-r-lg' src={''} alt='hero'/>
     </div>
   </div>
 </div>
