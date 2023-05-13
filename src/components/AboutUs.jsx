29045a2652a627c3194749102965064f10321d78
Vijay KV <vijaykv2228@gmail.com>
2023-05-13 15:18:00 +0530

    Update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
   <div class=" mt-3  ">
     <h1 class="text-4xl p-2  font-bold sm:text-7xl text-center sm:w-[700px]">We're changing the whole game.</h1>
     <div className='px-1 py-5  sm:ml-32 sm:mt-3 flex'>
-      <button className=' ml-4 outline-double outline-1 bg-black text-white py-2 px-6 rounded-full font-bold transition duration-300 ease-in-out hover:bg-white hover:text-black sm:w-44 sm:h-14'>Get Started</button>
+      <button className=' ml-6 outline-double outline-1 bg-black text-white py-2 px-6 rounded-full font-bold transition duration-300 ease-in-out hover:bg-white hover:text-black sm:w-44 sm:h-14'>Get Started</button>
       <button className='ml-4 outline-double outline-1 py-2 px-6 rounded-full font-bold transition duration-300 ease-in-out hover:bg-black hover:text-white sm:w-44 sm:h-14'>View Pricing</button>
     </div>
      {/* Desktop view */}
