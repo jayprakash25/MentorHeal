4c5096af8059a0571ab43bf4d10613d856458583
Vijay KV <vijaykv2228@gmail.com>
2023-05-17 20:26:16 +0530

    Updated

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -24,6 +24,22 @@ const AboutUs = () => {
 
   </div>
 </div>
+<div className='text-green-700 font-extrabold py-10 px-9 text-2xl'>OUR STORY</div>
+<div className='text-2xl font-bold px-9 -mt-6 w-[450px]'>We’re building the economic infrastructure for the internet. Businesses of every size .</div>
+<div className=' flex mt-3 px-7'>
+  <img  className='w-80 'src="https://i.ibb.co/C9Rh7t7/Capture.png" alt="" />
+  <p className=' font-light text-gray-600 ml-2 my-4 w-[1000px]'>A living place for curiosity and collaboration, meeting and meaning. The heart of creative community</p>
+</div>
+<h1 className='font-bold text-xl py-9 ml-9 '>1. 6 years of intense research</h1>
+<p className=' font-light text-gray-600 -mt-7 px-9 ml-4 w-[450px] '>Our founders had a solution, This is your Email marketing specialist, newsletter service and campaign manager, all rolled into one.</p>
+
+<h1 className='font-bold text-xl py-9 ml-9 '>2. Audience segmentation</h1>
+<p className=' font-light text-gray-600 -mt-7 px-9 ml-4 w-[450px] '>Improve audience engagement by segmenting. Boost you conversions by targeting subsets from you contacts. Give them content they need.</p>
+
+<h1 className='font-bold text-xl py-9 ml-9 '>3. Contact monitoring</h1>
+<p className=' font-light text-gray-600 -mt-7 px-9 ml-4 w-[450px] '>Just import your contact list, segment your contacts and we’ll get you a detailed real-time report of their activity.</p>
+
+
 </>
 
   )
