2d91d396ff9120f2d566e133e184cfa0f45ea8ae
Vijay KV <vijaykv2228@gmail.com>
2023-05-21 15:38:11 +0530

    Color of footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <div>
         
-<footer className="relative  bg-blue-950 text-white pt-8 pb-6">
+<footer className="relative  bg-blue-950 pt-8 pb-6">
   <div className="container mx-auto px-4">
     <div className="flex flex-wrap text-left lg:text-left">
-      <div className="w-full lg:w-6/12 px-4">
-        <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+      <div className="w-full lg:w-6/12 px-4  text-white">
+        <h4 className="text-3xl fonat-semibold text-blueGray-700  ">Let's keep in touch!</h4>
         <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
           Find us on any of these platforms, we respond 1-2 business days.
         </h5>
@@ -18,59 +18,59 @@ const Footer = () => {
       <div className="w-full lg:w-6/12 px-4">
         <div className="flex flex-wrap items-top mb-6">
           <div className="w-full lg:w-4/12 px-4 ml-auto">
-            <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">More</span>
+            <span className="block uppercase  text-white text-sm font-semibold mb-2">More</span>
             <ul className="list-unstyled">
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">About Us</a>
+                <a className="text-gray-400  hover:text-Gray-200 font-semibold block pb-2 text-sm" href="">About Us</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Our Bootcamp</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Our Bootcamp</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Meet Your Mentor</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Meet Your Mentor</a>
               </li>
               
             </ul>
           </div>
           <div className="w-full lg:w-4/12 px-4 ml-auto">
-            <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Mentorship Options</span>
+            <span className="block uppercase  text-white text-sm font-semibold mb-2">Mentorship Options</span>
             <ul className="list-unstyled">
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Health</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Health</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Career</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Career</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Learning</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Learning</a>
               </li>
             
             </ul>
           </div>
           <div className="w-full lg:w-4/12 px-4">
-            <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+            <span className="block uppercase  text-white text-sm font-semibold mb-2">Other Resources</span>
             <ul className="list-unstyled">
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Support</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Support</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Terms &amp; Conditions</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Terms &amp; Conditions</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Privacy Policy</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Privacy Policy</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Contact Us</a>
+                <a className="text-gray-400 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Contact Us</a>
               </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
-    <hr className="my-6 border-blueGray-300"/>
+    <hr className="my-6 border-blueGray-300 "/>
     <div className="flex flex-wrap items-center md:justify-between justify-center">
       <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-        <div className="text-sm text-blueGray-500 font-semibold py-1">
+        <div className="text-sm  text-white font-semibold py-1">
           Copyright © <span id="get-current-year">2023</span><a href="/" className="text-blueGray-500 "> mentorrnow</a>
           
         </div>
