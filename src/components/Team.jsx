ce6c38f7b825998f4d7007ddebd36dfd254d70b3
unknown <jayprakashpj2525@gmail.com>
2023-05-23 11:04:53 +0530

    fix

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,11 +2,12 @@ import TeamCard from "./TeamCard";
 
 const Team = () => {
     return (
-        <div>
+        <div  className="">
         <div className="flex items-center justify-center">
             <h1 className="font-bold text-[34px] md:text-[41px] lg:text-[48px] leading-10 md:leading-[44px] lg:leading-none  text-center md:w-[487px]">Our Leading, Strong And Creative team</h1>
         </div>
-        <div className="grid grid-cols-2  md:grid-cols-3 justify-between m-8 gap-4 ">
+        <div className="md:grid flex flex-col justify-center   md:grid-cols-3  items-center m-8 gap-4 ">
+
         <TeamCard name = "James Bond" role="Developer"/>
         <TeamCard name = "James Bond" role="Developer"/>
         <TeamCard name = "James Bond" role="Developer"/>
--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -4,7 +4,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const TeamCard = (props) => {
     return (
-        <div className="border-[0.44px] rounded-xl border-[#000000] w-[188px] justify-center lg:w-[90%] lg:h-[450px] flex flex-col items-center p-2 pt-3 lg:pt-0 shadow-lg">
+        <div className="border-[0.44px] rounded-xl border-[#000000] w-[200px] justify-center lg:w-[90%] lg:h-[450px] flex flex-col items-center p-2 pt-3 lg:pt-0 shadow-lg">
         <img src={Profile} className="lg:w-[65%]"/>
         <div className="flex flex-col justify-center items-center gap-1 mt-[9px] lg:mt-5 ">
             <h1 className="font-inter text-[14px] lg:text-[28px] ">{props.name}</h1>
