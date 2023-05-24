0148a4b15c58a7ce3e79dca9c95bb50c689a96d7
unknown <jayprakashpj2525@gmail.com>
2023-05-24 14:43:33 +0530

    minor fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,17 +7,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className='  w-full  '>
-      <div className='flex justify-between items-center  max-w-7xl px-9 py-8 gap-4'>
+    <nav className='    '>
+      <div className='flex justify-between items-center  px-9 py-8 gap-4'>
 
         <div className='flex '>
           <Link to='/' className='flex'  onClick={() => {
       window.scrollTo(0,0)
     }}><img className='w-[200px] mt-2'src="https://i.ibb.co/pr6nQzZ/final-logo.png" alt="final-logo" border="0"/></Link>
         </div>
-<div className=' hidden lg:flex gap-20  items-center'>
-          <ul className='list-none flex flex-row  gap-14'>
-            <li className='cursor-pointer hover:text-zinc-400  transition ease-in-out duration-300'>Mentorship Options</li>
+<div className=' hidden lg:flex   items-center'>
+          <ul className='list-none flex flex-row  space-x-5 mr-6'>
+            <li className='cursor-pointer hover:text-zinc-400  transition ease-in-out duration-300'>Mentorship-Options</li>
             <li className='cursor-pointer hover:text-zinc-400  transition ease-in-out duration-300'>How it works</li>
             <Link to='/about'><li className='cursor-pointer hover:text-zinc-400  transition ease-in-out duration-300'>About us</li></Link>
             <li className='cursor-pointer hover:text-zinc-400  transition ease-in-out duration-300'>Assessment</li>
