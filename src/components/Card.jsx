0e52a8c0166cb8e8595e8a7b487b9a4a9796467c
Rahul <chrahulofficial@gmail.com>
2023-07-20 23:02:25 +0530

    DONE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = () => {
     AOS.init({ duration: 2000 });
   }, []);
   return (
-    <div className="flex flex-col items-center my-16 justify-center gap-5 md:grid grid-cols-3 md:px-5">
+    <div className="flex flex-col items-center justify-center grid-cols-3 gap-5 my-16 md:grid md:px-5">
       {Carddata.map((item, index) => {
         return (
           <Link
@@ -19,7 +19,7 @@ export const Card = () => {
               Blog: item.Blog,
             }}
           >
-            <div className="flex justify-center hover:scale-105 duration-300 ease-in-out cursor-pointer">
+            <div className="flex justify-center duration-300 ease-in-out cursor-pointer hover:scale-105">
               <div
                 data-aos="fade-up"
                 data-aos-easing="ease-in-out"
@@ -29,33 +29,14 @@ export const Card = () => {
               >
                 <img
                   src={item.image}
-                  className="bg-white py-2  w-32 rounded-full mt-4"
+                  className="w-32 py-2 mt-4 bg-white rounded-full"
                 />
-<div
-              data-aos = "zoom-in"
-               className="max-w-[80vw] md:w-[30vw] shadow-sm border-[0.3px] border-gray-200 rounded-lg flex flex-col items-center space-y-5 p-5">
-                <img src={item.image} className="bg-white py-2  w-44  rounded-full mt-4"/>
-                <h1 className="text-3xl font-bold ">{item.Title}</h1>
-                <p className="text-center text-[#8ca1b3] text-sm leading-6">
-                  {item.Para}
-                </p>
-                <p className=" cursor-pointer  text-[#00b8d3]">Read More</p>
-                <div
-                  data-aos="zoom-in"
-                  className="max-w-[80vw] md:w-[30vw] shadow-sm border-[0.3px] border-gray-200 rounded-lg flex flex-col items-center space-y-5 p-5"
-                >
-                  <img
-                    src={item.image}
-                    className="bg-white py-2  w-44  rounded-full mt-4"
-                  />
                   <h1 className="text-3xl font-bold ">{item.Title}</h1>
                   <p className="text-center text-[#8ca1b3] text-sm leading-6">
                     {item.Para}
                   </p>
                   <p className=" cursor-pointer  text-[#00b8d3]">Read More</p>
-                </div>
               </div>
-            </div>
             </div>
           </Link>
         );
@@ -71,16 +52,16 @@ export const FirstCard = () => {
   return (
     <div className="px-4 bg-[#DAFFFB] ">
       <section className=" my-28">
-        <div className=" md:flex md:flex-row-reverse p-7 md:justify-around  items-center md:space-x-5 md:px-5">
+        <div className="items-center md:flex md:flex-row-reverse p-7 md:justify-around md:space-x-5 md:px-5">
           <div
             data-aos="fade-right"
             data-aos-easing="ease-in-out"
             data-aos-once="true"
-            className="space-y-8 flex py-5 flex-col text-center md:text-left order-2"
+            className="flex flex-col order-2 py-5 space-y-8 text-center md:text-left"
           >
             <h1 className="text-2xl md:text-4xl md:max-w-[50vw] leading-8 font-semibold">
               How will{" "}
-              <span className="bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 text-transparent  bg-clip-text font-bold">
+              <span className="font-bold text-transparent bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 bg-clip-text">
                 Mentorship
               </span>{" "}
               help you?
