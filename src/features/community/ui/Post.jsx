2cced7ee34235c716e598e50215681001a879b14
unknown <jayprakashpj2525@gmail.com>
2023-10-27 13:47:31 +0530

    post

--- src/features/community/ui/Post.jsx
+++ src/features/community/ui/Post.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { addDoc, collection } from "firebase/firestore";
 import { BsPeople } from "react-icons/bs";
+import { db } from "../../../Firebase";
+import { useState } from "react";
 
 export default function Post() {
+  const [data, setData] = useState({
+    post: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "POSTS"), data);
+      alert("Done");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="pl-2 mt-6 space-y-2">
@@ -20,14 +36,24 @@ export default function Post() {
           cols={20}
           rows={12}
           type="text"
+          value={data.post}
+          onChange={(e) => {
+            setData({
+              ...data,
+              post: e.target.value,
+            });
+          }}
           className=" mx-auto text-lg w-[70vw] sm:w-[60vw] md:w-[50vw] lg:w-[45vw]  border-b-2 border-gray-300 outline-none"
           placeholder="Say Something .."
         />
-        <div class="flex justify-end gap-5">
+        <div className="flex justify-end gap-5">
           <button className="px-5 font-semibold text-white cursor-pointer bg-gradient-to-l from-cyan-400 via-cyan-400 to-cyan-300 py-1.5 rounded-full">
             Cancel
           </button>
-          <button className="px-5 font-semibold text-white cursor-pointer bg-gradient-to-l from-cyan-400 via-cyan-400 to-cyan-300 py-1.5 rounded-full">
+          <button
+            onClick={handleSubmit}
+            className="px-5 font-semibold text-white cursor-pointer bg-gradient-to-l from-cyan-400 via-cyan-400 to-cyan-300 py-1.5 rounded-full"
+          >
             Submit
           </button>
         </div>
