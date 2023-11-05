9908f15a957e953f1e8732e10452102cc1019e2a
Rahul <chrahulofficial@gmail.com>
2023-11-05 11:47:45 +0530

    ui fixed

--- src/features/BecomeMentor/JoinMentor.jsx
+++ src/features/BecomeMentor/JoinMentor.jsx
@@ -90,7 +90,7 @@ export default function JoinMentor({ setjoinmentor }) {
   return (
     <>
       <div className="fixed inset-0 z-50 flex items-center justify-center h-full p-3 overflow-y-scroll bg-black bg-opacity-70 backdrop-blur animate__animated animate__fadeIn">
-        <div className="w-[80vw] md:w-[60vw] lg:w-[45vw] px-10 bg-white rounded-md py-3 overflow-y-scroll ">
+        <div className="w-[92vw] md:w-[60vw] lg:w-[45vw] px-8 bg-white rounded-md py-3 overflow-y-scroll ">
           <div className="flex justify-end">
             <RxCross2
               onClick={() => {
