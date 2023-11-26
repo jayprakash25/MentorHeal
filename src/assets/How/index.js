1ca755d2ae5d0670778c508606dc163990960948
unknown <jayprakashpj2525@gmail.com>
2023-11-26 11:17:27 +0530

    pushed

---
+++ src/assets/How/index.js
@@ -0,0 +1,6 @@
+import chose from "./Chose.png";
+import Find from "./Find.png";
+import Schedule from "./Schedule.png";
+import Set from "./Set.png";
+
+export { chose, Set, Find, Schedule };
--- src/components/HowContent.jsx
+++ src/components/HowContent.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import { Card } from "./Card";
+import { chose, Find, Schedule, Set } from "../assets/How";
 
 const FAQs = ({ Q, children }) => {
   const [open, setOpen] = useState(false);
@@ -61,40 +62,39 @@ const HowContent = () => {
   ];
 
   return (
-    <div className="pt-24 p-8">
+    <div className="pt-24 p-8 ">
       <h1 className="text-3xl lg:text-5xl  text-center pb-14 font-semibold">
         How it Works?
       </h1>
-      <p className="text-justify font-medium">
+      <p className="text-justify font-medium lg:text-xl">
         Unlock your full potential with our easy 4-step process. We&apos;ve
         streamlined every aspect of your journey, ensuring a seamless experience
         from start to finish. Our platform is your gateway to personal growth
         and achievement, where you&apos;ll connect with experienced mentors who
         are passionate about helping you reach your goals.
       </p>
-      <div className="space-y-6">
-        <div className="flex flex-col md:flex-row items-center md:justify-around">
-          <img src={path} className="max-w-sm md:max-w-md" />
+      <div className="space-y-6 pt-6 lg:pt-10">
+        <div className="flex flex-col md:flex-row items-center space-y-4 lg:space-y-0 md:justify-between">
+          <img src={Set} className="w-[16rem] lg:w-[24rem]" />
           <div className="py-2 space-y-2">
             <h1 className="bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 text-transparent  bg-clip-text font-bold text-2xl lg:text-3xl ">
-              1. Select a Category
+              1. Get Started: Set Up Your Account
             </h1>
-            <p className="max-w-sm text-[#7C7C7C] text-[13px] md:text-[16.5px] lg:text-[20px] leading-5 md:leading-7 lg:leading-8 md:max-w-md">
-              Go to the category relevant to you. Select one with a journey most
-              relevant to yours.
+            <p className="max-w-sm text-[#7C7C7C]  text-[16.5px] lg:text-[20px] leading-5 font-semibold md:leading-7 lg:leading-8 md:max-w-md">
+              Unlock the door to your journey with just a few clicks.
             </p>
           </div>
         </div>
 
-        <div className="flex flex-col items-center md:flex-row-reverse md:justify-around">
-          <img src={booking} className="max-w-sm md:max-w-md" />
+        <div className="flex flex-col items-center md:flex-row-reverse space-y-4 lg:space-y-0  md:justify-around">
+          <img src={chose} className="w-[16rem]" />
           <div className="py-2 space-y-2">
             <h1 className=" text-2xl bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 text-transparent  bg-clip-text font-bold  lg:text-3xl">
-              2. Book a Slot
+              2. Explore and Discover: Chose a category
             </h1>
             <p className="max-w-sm  text-[#7C7C7C] text-[13px] md:text-[16.5px] lg:text-[20px] leading-5 md:leading-7 lg:leading-8 md:max-w-md">
-              Click &apos;Schedule your time&apos; and then use the calender to
-              book a slot that is convenient to you.{" "}
+              Dive into a world of possibilities by selecting your area of
+              mentorship.
             </p>
           </div>
         </div>
@@ -103,11 +103,22 @@ const HowContent = () => {
           <img src={meet} className="max-w-sm md:max-w-md" />
           <div className="py-2 space-y-2">
             <h1 className=" bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 text-transparent  bg-clip-text font-bold  text-2xl text-[#00b8b3] lg:text-3xl">
-              3. Join the Session
+              3. Meet Your Guide: Find The Right Mentor
             </h1>
             <p className="max-w-sm  text-[#7C7C7C] text-[13px] md:text-[16.5px] lg:text-[20px] leading-5 md:leading-7 lg:leading-8 md:max-w-md">
-              Once booked, you will receive a video link in email/on whatsapp
-              for the selected time slot with your Mentor.
+              Connect with experienced mentors which you feel right for you
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center md:flex-row-reverse md:justify-around">
+          <img src={booking} className="max-w-sm md:max-w-md" />
+          <div className="py-2 space-y-2">
+            <h1 className=" text-2xl bg-gradient-to-l from-cyan-500 via-cyan-300 to-cyan-500 text-transparent  bg-clip-text font-bold  lg:text-3xl">
+              4. Book Your Path: Schedule A Call
+            </h1>
+            <p className="max-w-sm  text-[#7C7C7C] text-[13px] md:text-[16.5px] lg:text-[20px] leading-5 md:leading-7 lg:leading-8 md:max-w-md">
+              Take the next step towards success by scheduling your first
+              empowering conversation.
             </p>
           </div>
         </div>
