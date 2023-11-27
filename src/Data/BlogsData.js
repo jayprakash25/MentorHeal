c67679f797b178b28c5480bee3235368a7bd97b9
Rahul <chrahulofficial@gmail.com>
2023-11-27 18:16:43 +0530

    fxed

--- src/Data/BlogsData.js
+++ src/Data/BlogsData.js
@@ -80,87 +80,9 @@ const AllBlogs = [
       },
     ],
   },
-  {
-    Category: ["Tech", "Life", "Health"],
-    Blogimage:
-      "https://images.pexels.com/photos/5668668/pexels-photo-5668668.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    BLogTittle:
-      "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-    BlogSections: [
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-    ],
-  },
-  {
-    Category: ["Tech", "Life", "Health"],
-    Blogimage:
-      "https://images.pexels.com/photos/5668668/pexels-photo-5668668.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    BLogTittle:
-      "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-    BlogSections: [
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-    ],
-  },
-  {
-    Category: ["Tech", "Life", "Health"],
-    Blogimage:
-      "https://images.pexels.com/photos/5668668/pexels-photo-5668668.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    BLogTittle:
-      "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-    BlogSections: [
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-      {
-        SectionTittle:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-        SectionPara:
-          "Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!Officiis velit sapiente nisi veritatis rem quisquam beatae sequi sunt iusto! Maiores, accusamus neque!",
-      },
-    ],
-  },
+  
+  
+  
   
 ];
 
