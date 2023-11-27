52e4b4ce5d1f5c9c8617edb4c2cea4ca9bd26360
Rahul <chrahulofficial@gmail.com>
2023-11-27 19:39:24 +0530

    dn

--- src/Data/JoinPeerNetwork.jsx
+++ src/Data/JoinPeerNetwork.jsx
@@ -7,12 +7,12 @@ const Data = [
     {
       image : "https://www.soulup.in/cdn/shop/files/Website.jpg?v=1672727241",
       Tittle : "Grow yourself",
-      Para : "Reinforce your strength when you have SoulUp conversations. Progress further using our specially designed programs."
+      Para : "Reinforce your strength when you have MentorHeal conversations. Progress further using our specially designed programs."
     },
     {
       image : "https://www.soulup.in/cdn/shop/files/Website_copy_2.jpg?v=1672728094",
       Tittle : "Access benefits",
-      Para : "Get a share of the value that the platform creates. Access free workshops, trainings and discounts on SoulUp products"
+      Para : "Get a share of the value that the platform creates. Access free workshops, trainings and discounts on MentorHeal products"
     },
 ]
 
